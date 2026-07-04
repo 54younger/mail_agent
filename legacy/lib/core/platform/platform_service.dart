3582e3182bb0ee758afb55ab34639c01379e03f2
54younger <chenyang202409@163.com>
2026-07-04 08:52:44 +0200

    rm flutter

--- legacy/lib/core/platform/platform_service.dart
+++
@@ -1,7 +0,0 @@
-abstract class PlatformService {
-  Future<String> getAppDocumentsPath();
-  Future<int> getAvailableMemoryMb();
-  int getLogicalCpuCount();
-  bool get isDesktop;
-  bool get isMobile;
-}
